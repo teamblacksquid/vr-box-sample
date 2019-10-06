86bcde8f9959126fdcdd786aacfca2774645dc89
Jake LEE <jakeleedev@outlook.com>
2019-10-06 14:41:37 +0900

    first commit

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float rotateForce;
+    private bool isReverse;
+
+    private void Update()
+    {
+        transform.Rotate(0, rotateForce * Time.deltaTime * (isReverse ? -1 : 1), 0) ;
+        
+    }
+
+    public void Reverse(bool reverse)
+    {
+        isReverse = reverse;
+    }
+
+}

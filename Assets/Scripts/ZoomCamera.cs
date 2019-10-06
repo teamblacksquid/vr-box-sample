117c50baa38baa3f9e3d3dbd8128adb7ed396ac0
BA78 <lee53728433@gmail.com>
2019-10-06 17:27:26 +0900

    Zoom

---
+++ Assets/Scripts/ZoomCamera.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomCamera : MonoBehaviour
+{
+    public float zoomSpeed = 10f;
+    public float fieldOfView = 60f;
+    public float maxZoomFOV = 20f;
+
+
+    private bool wheelZooming = false;
+    private Camera thisCamera;
+
+    private void Awake()
+    {
+        thisCamera = GetComponent<Camera>();
+
+    }
+
+    void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        WheelZoomInAndOut();
+        Zoom();
+    }
+
+    private void WheelZoomInAndOut()
+    {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        if (scroll != 0)
+        {
+            wheelZooming = true;
+        }
+
+        if (thisCamera.fieldOfView <= maxZoomFOV && scroll > 0)
+        {
+            thisCamera.fieldOfView = maxZoomFOV;
+        }
+        else if (thisCamera.fieldOfView >= fieldOfView && scroll < 0)
+        {
+
+        }
+        else
+        {
+            thisCamera.fieldOfView -= scroll * zoomSpeed;
+        }
+    }
+
+    private void Zoom()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            wheelZooming = false;
+        }
+
+        if (wheelZooming == false)
+        {
+            if (Input.GetButton("Fire2"))
+            {
+                thisCamera.fieldOfView = maxZoomFOV;
+            }
+            else
+            {
+                thisCamera.fieldOfView = fieldOfView;
+            }
+        }
+
+    }
+
+    void LateUpdate()
+    {
+
+    }
+}
